03e6d2529549b69c27e8282b37272338dda6371d
Ivanilton Neto <netijua@gmail.com>
2022-12-18 21:50:25 -0300

    fix: fineshed

--- bandkamp/urls.py
+++ bandkamp/urls.py
@@ -1,5 +1,4 @@
 """bandkamp URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
@@ -15,10 +14,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("albums.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema"))
     # path("api/", include("songs.urls")),
 ]
--- songs/migrations/0001_initial.py
+++ songs/migrations/0001_initial.py
@@ -9,17 +9,35 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('albums', '0002_alter_album_options'),
+        ("albums", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Song',
+            name="Song",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('duration', models.CharField(max_length=255)),
-                ('album', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='songs', to='albums.album')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("duration", models.CharField(max_length=255)),
+                (
+                    "album",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="songs",
+                        to="albums.album",
+                    ),
+                ),
             ],
+            options={
+                "ordering": ("id",),
+            },
         ),
     ]
